16829851c8144138c72905e7a49a1538b74f888f
Isak Berg Endresen <isakbendresen@gmail.com>
2023-01-24 21:06:30 +0100

    Added Bootstrap to Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Navbar from "./components/navbar";
-import Card from "./components/cards";
+import CardComponent from "./components/CardComponent";
 import Button from "react-bootstrap/Button";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -10,8 +9,15 @@ function App() {
       <div className="App">
         <Navbar></Navbar>
       </div>
-      <div>
-        <Card title="test" text="testing testing" />
+      <div className="card-container">
+        <CardComponent title="Abakus.no">Denne kjenner du til</CardComponent>
+        <CardComponent title="Opptak">Lykke til!</CardComponent>
+        <CardComponent title="Statutter">Lover og regler, må følges</CardComponent>
+        <CardComponent title="Status">Sjekk om Abakus.no funker</CardComponent>
+        <CardComponent title="Ny i Trondheim">Ny i byen, da MÅ du trykk her!</CardComponent>
+        <CardComponent title="Veldedighetsfest Auksjon">Drikk øl og hjelp verden</CardComponent>
+        <CardComponent title="Abakus Saker">Diskuter saker i Abakus</CardComponent>
+        <CardComponent title="Placeholder">Bla bla bla</CardComponent>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
