fd3b7adb37e87fa69666c61a667f3d6739f888f2
Jonas de Luna Skulberg <jonasdls@gmail.com>
2023-01-24 20:02:49 +0100

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Card from "./components/cards";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div>
+    <div className="container-fluid">
       <div className="App">
         <Navbar></Navbar>
       </div>
       <div>
-        <Card title="test" text="testing testing"/>
+        <Card title="test" text="testing testing" />
       </div>
     </div>
   );
