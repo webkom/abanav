19f2c4280041b2f63ff986f2971e02dd8f3a53ed
Jonas de Luna Skulberg <jonasdls@gmail.com>
2023-02-01 21:41:29 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/navbar";
 import SiteViews from "./components/SiteViews";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="container">
       <Header />
       <SiteViews></SiteViews>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+    <footer className="footer">&copy; Abakus {new Date().getFullYear()}</footer>
+  );
+}
+
+export default Footer;
