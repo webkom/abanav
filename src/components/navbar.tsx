c612099dbc9d38b9f355053cd646ee22fa62e1fb
Jonas de Luna Skulberg <jonasdls@gmail.com>
2023-02-01 21:52:24 +0100

    remove redundant links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,13 +15,6 @@ function Header() {
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#">Hjem</Nav.Link>
-            <Nav.Link href="#">Om</Nav.Link>
-            <Nav.Link href="https://abakus.no">Abakus.no</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
